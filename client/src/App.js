9dddaf892da4ae508b486b6b4031ecb88215915e
Christopher Abedi <chris@ChristophersMBP.hsd1.fl.comcast.net>
2018-04-16 19:00:36 -0400

    init and Material-Ui AutoComplete

---
+++ client/src/App.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+
+import AutoComplete from 'material-ui/AutoComplete';
+
+import AppBar from 'material-ui/AppBar';
+
+
+import logo from './logo.svg';
+
+import './App.css';
+
+
+const fruit = [
+  'Apple', 'Apricot', 'Avocado',
+  'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry',
+  'Boysenberry', 'Blood Orange',
+  'Cantaloupe', 'Currant', 'Cherry', 'Cherimoya', 'Cloudberry',
+  'Coconut', 'Cranberry', 'Clementine',
+  'Damson', 'Date', 'Dragonfruit', 'Durian',
+  'Elderberry',
+  'Feijoa', 'Fig',
+  'Goji berry', 'Gooseberry', 'Grape', 'Grapefruit', 'Guava',
+  'Honeydew', 'Huckleberry',
+  'Jabouticaba', 'Jackfruit', 'Jambul', 'Jujube', 'Juniper berry',
+  'Kiwi fruit', 'Kumquat',
+  'Lemon', 'Lime', 'Loquat', 'Lychee',
+  'Nectarine',
+  'Mango', 'Marion berry', 'Melon', 'Miracle fruit', 'Mulberry', 'Mandarine',
+  'Olive', 'Orange',
+  'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Physalis', 'Plum', 'Pineapple',
+  'Pumpkin', 'Pomegranate', 'Pomelo', 'Purple Mangosteen',
+  'Quince',
+  'Raspberry', 'Raisin', 'Rambutan', 'Redcurrant',
+  'Salal berry', 'Satsuma', 'Star fruit', 'Strawberry', 'Squash', 'Salmonberry',
+  'Tamarillo', 'Tamarind', 'Tomato', 'Tangerine',
+  'Ugli fruit',
+  'Watermelon',
+];
+
+
+
+class App extends Component {
+  state = {
+    request: '',
+    response: '',
+    searchText: ''
+  };
+
+  handleSelect (t) { this.setState( { searchText: t }) }
+
+
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <AppBar
+        title="FruitPicker"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+        <p className="App-intro">{this.state.response}</p>
+
+        <AutoComplete
+      floatingLabelText="Type 'peah', fuzzy search"
+      filter={AutoComplete.fuzzyFilter}
+      dataSource={fruit}
+      maxSearchResults={5}
+      onNewRequest={this.handleSelect.bind(this)}
+
+    />
+    <h1> {this.state.searchText} </h1>
+      </div>
+
+    );
+  }
+}
+
+export default App;
